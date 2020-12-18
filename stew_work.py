78aa6c64c938a2b3b35e719b00b6fc0e6c0540c1
towles-osu <towles@oregonstate.edu>
2020-12-17 16:34:05 -0800

    started making simple classes for things

---
+++ stew_work.py
@@ -0,0 +1,27 @@
+# Author:Stew
+# Date: 12/16/2020
+# Description: This is where I am writing stuff so I don't mess up main
+
+import Random
+
+class Character():
+    """This is the class that defines player characters"""
+
+    def __init__(self, hp=10, atk=1):
+        """this initializes the player character"""
+        self.hp = hp
+        self.atk = atk
+        self.max_hp = hp
+
+    def take_dmg(self, dmg):
+        """takes an int amount of damage dealt to character"""
+        self.hp -= dmg
+
+    def get_hp(self):
+        """returns the current amount of health"""
+        return self.hp
+
+    def get_max_hp(self):
+        """returns the max health of character"""
+        return self.max_hp
+
